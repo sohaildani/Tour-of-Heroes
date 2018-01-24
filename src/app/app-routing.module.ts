ac0bc76f12517121b99957427407054c104fdd06
Mohd Sohail Khan <Mohd Sohail Khan>
2018-01-24 23:51:29 +0530

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailsComponent }  from './hero-details/hero-details.component';
+
+const routes: Routes=[
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path:'heroes',component:HeroesComponent},
+  { path: 'details/:id', component: HeroDetailsComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
